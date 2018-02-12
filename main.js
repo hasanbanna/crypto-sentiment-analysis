142715d07d31f8086f4d7f09b756360e56a90105
Hasan Banna <banna.hsn@gmail.com>
2018-02-12 14:52:17 +0000

    added vuejs

--- main.js
+++ main.js
@@ -1,11 +1,29 @@
+let credentials = require('./credentials').credentials;
+let Twitter = require("twitter");
+let client = new Twitter({
+  consumer_key: credentials.consumer_key,
+  consumer_secret: credentials.consumer_secret,
+  access_token_key: credentials.access_token_key,
+  access_token_secret: credentials.access_token_secret
+});
+
+let params = { q: 'Tron', lang: 'en', count: '100'};
 let sentiment = require('sentiment');
 
-let a = ["Substratum builds the decentralized web by leveraging the power of the individual. Anyone can publish, anyone can participate, and everyone is welcome to browse.",
-"Because there's no single authority delivering or monitoring content, censorship and geo-restricted sites won't be an issue on the decentralized web. It doesn't matter where you live or what content you're accessing, everyone in the world sees the exact same content. Net neutrality laws threaten to regulate internet access by charging higher rates for viewing certain content. All sites are treated the same on the decentralized web, allowing users to enjoy the full online experience without lining the pockets of internet service providers."]
 let averageScore = 0;
-for(let i = 0; i < a.length; i++){  
-  console.log("positive: ", sentiment(a[i]).positive);
-  console.log("negative: ", sentiment(a[i]).negative);
-  averageScore += sentiment(a[i]).score;
-}
-console.log("average score: ", averageScore / a.length);
+client.get('search/tweets', params, function(error, tweets, response) {
+  if (!error) {
+    for(tweet in tweets['statuses']){
+      let currentTweet = tweets['statuses'][tweet].text;
+      let tweetSentimentScore = sentiment(currentTweet).score;
+      console.log("tweet: ",currentTweet, "score: ",tweetSentimentScore);
+      averageScore += tweetSentimentScore
+    }
+    console.log("average score: ", averageScore / tweets['statuses'].length);
+    // console.log(tweets);
+  }else{
+    console.log('error: ', error);
+  }
+});
+
+
